982248c7f39f0fe704ac59faf33ae89d1d1411da
ClaudiaAgos <43794315+ClaudiaAgos@users.noreply.github.com>
2018-10-04 17:57:57 +0200

    Add files via upload
    TEMPORANEO, erano i messaggi che avevo pensato delle carte prima di fare le domande (non le ho ancora controllate pardon)

---
+++ messaggi.cpp
@@ -0,0 +1,28 @@
+// TESTATO OK   messaggi relativi alle carte o alle caselle tabellone
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string  msg_carte(int num){
+	string	str;
+	switch (num){
+		case 0:	str ="ritira il dado";
+				break;
+		case 1:  str= "salta il turno";	
+		  	    break;
+		case 2:  str= "ritorna alla casell 0";	
+		  	    break;
+		case 3:  str= "fai 3 passi in avanti";	
+		  	    break;
+		case 4:  str= "fai 3 passi indietro";	
+		  	    break;
+		case 5:  str= "scambia il posto con il giocatore precedente";	
+		  	    break;
+		default: str="errore" 	;
+		
+		 	    
+	}
+	return str;
+}
