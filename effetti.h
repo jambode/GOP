d0ea893b159343b948cc1bcc38767496c022c76a
Martina <matry98@live.it>
2018-10-16 11:44:28 +0200

    Update effetti.h

--- effetti.h
+++ effetti.h
@@ -1,22 +1,30 @@
-#ifndef EFFETTI_H
-#define EFFETTI_H
+#ifndef _EFFETTI_H
+#define _EFFETTI_H
 #include<iostream>
 #include "dado.h"
 #include "mazzo_carte.h"
 #include "player.h"
+#include "carta.h"
 using namespace std;
 
 class effetti{
-	dado d;
+
 	player *p[100];
+	mazzo m;
+    carta card;
+
 	public:
+		//mazzo	
+		dado d;
+	    int num_estratto; 
 		effetti(){
 		};
 		int tira_di_nuovo();
 		int vai_avanti(int pos);
-		void salta_turno();   //inserire un contatore di turni??
+		void salta_turno();   //inserire un contatore di turni??  forse non lo implemento
 		void pesca_carta();
+	    int fortuna(int num);
 	};
-		
 
-#endif 
+
+#endif /*EFFETTI_H*/
