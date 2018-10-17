f6a8417c8d704c53cd961f37887abae7a75e119c
ClaudiaAgos <43794315+ClaudiaAgos@users.noreply.github.com>
2018-10-18 01:49:42 +0200

    Update domande.cpp

--- domande.cpp
+++ domande.cpp
@@ -1,75 +1,76 @@
 #include "carta.h"
 #include "domande.h"
+#include "effetti.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 	domanda vect_dom[NUM_TOT_DOM]={
-	/*1° domanda*/{"Se lo alimenti vive, se gli dai da bere muore ",/* lista risposte*/{
+	/*1Â° domanda*/{"Se lo alimenti vive, se gli dai da bere muore ",/* lista risposte*/{
 						" a - Gas ",
 						" b - Fuoco ",
 						" c - Caldo",
-						" d - Fumo"	}, /* fine lista risposte 1° domanda*/ 
-		/*risposta esatta*/ 'b'},/*fine 1° domanda
+						" d - Fumo"	}, /* fine lista risposte 1Â° domanda*/ 
+		/*risposta esatta*/ 'b'},/*fine 1Â° domanda
 						
-	/*2° domanda*/{"Piange senza voce, Fluttua senza ali, Morde senza denti, Mormora senza bocca. ", /* lista risposte*/{
+	/*2Â° domanda*/{"Piange senza voce, Fluttua senza ali, Morde senza denti, Mormora senza bocca. ", /* lista risposte*/{
 						" a - La Frana",
 						" b - Il tornado",
 						" c - Il vento",
-						" d - La valanga"	}, /* fine lista risposte 2° domanda*/ 
-		/*risposta esatta*/ 'c'},/*fine 2° domanda
+						" d - La valanga"	}, /* fine lista risposte 2Â° domanda*/ 
+		/*risposta esatta*/ 'c'},/*fine 2Â° domanda
 		
-	/*3° domanda*/{"Mi muovo in cerchi, Ma vado sempre avanti. Non mi lamento mai, Non importa dove sono condotta. ", /* lista risposte*/{
+	/*3Â° domanda*/{"Mi muovo in cerchi, Ma vado sempre avanti. Non mi lamento mai, Non importa dove sono condotta. ", /* lista risposte*/{
 						" a - La ruota",
 						" b - La girandola",
 						" c - L'aquilone",
-						" d - Il ventilatore"	}, /* fine lista risposte 2° domanda*/ 
-		/*risposta esatta*/ 'a'},/*fine 3° domanda
+						" d - Il ventilatore"	}, /* fine lista risposte 2Â° domanda*/ 
+		/*risposta esatta*/ 'a'},/*fine 3Â° domanda
 		
-	/*4° domanda*/{"Ha radici che nessuno vede. Supera ogni albero. Sale verso l'alto ma non cresce mai. ", /* lista risposte*/{
+	/*4Â° domanda*/{"Ha radici che nessuno vede. Supera ogni albero. Sale verso l'alto ma non cresce mai. ", /* lista risposte*/{
 						" a - La roccia",
 						" b - La collina",
 						" c - La quercia",
-						" d - La montagna"	}, /* fine lista risposte 2° domanda*/ 
-		/*risposta esatta*/ 'd'},/*fine 4° domanda	
+						" d - La montagna"	}, /* fine lista risposte 2Â° domanda*/ 
+		/*risposta esatta*/ 'd'},/*fine 4Â° domanda	
 
-	/*5° domanda*/{"Vado avanti vado indietro corro e mi fermo ma non cambio mai di posto. Chi sono? ", /* lista risposte*/{
+	/*5Â° domanda*/{"Vado avanti vado indietro corro e mi fermo ma non cambio mai di posto. Chi sono? ", /* lista risposte*/{
 						" a - La lancetta",
 						" b - La freccia",
 						" c - Il pendolo",
-						" d - Il treno"	}, /* fine lista risposte 2° domanda*/ 
-		/*risposta esatta*/ 'c'},/*fine 5° domanda
+						" d - Il treno"	}, /* fine lista risposte 2Â° domanda*/ 
+		/*risposta esatta*/ 'c'},/*fine 5Â° domanda
 	
-	/*6° domanda*/{"Nel calendario alcuni mesi hanno 30 giorni, altri 31. Quanti mesi hanno 28 giorni? ", /* lista risposte*/{
+	/*6Â° domanda*/{"Nel calendario alcuni mesi hanno 30 giorni, altri 31. Quanti mesi hanno 28 giorni? ", /* lista risposte*/{
 						" a - Nessuno",
 						" b - Uno",
 						" c - Tutti",
-						" d - Quattro"	}, /* fine lista risposte 2° domanda*/ 
-		/*risposta esatta*/ 'c'},/*fine 6° domanda	
+						" d - Quattro"	}, /* fine lista risposte 2Â° domanda*/ 
+		/*risposta esatta*/ 'c'},/*fine 6Â° domanda	
 
-	/*7° domanda*/{"Tutti mi sanno saprire, nessuno mi sa richiudere, non sto in piedi dritto e se mi rompi, a volte, sono fritto, chi sono?", /* lista risposte*/{
+	/*7Â° domanda*/{"Tutti mi sanno saprire, nessuno mi sa richiudere, non sto in piedi dritto e se mi rompi, a volte, sono fritto, chi sono?", /* lista risposte*/{
 						" a - L'uovo",
 						" b - La busta",
 						" c - La bottiglia",
-						" d - Il cocco"	}, /* fine lista risposte 2° domanda*/ 
-		/*risposta esatta*/ 'a'},/*fine 7° domanda	
+						" d - Il cocco"	}, /* fine lista risposte 2Â° domanda*/ 
+		/*risposta esatta*/ 'a'},/*fine 7Â° domanda	
 		
-	/*8° domanda*/{"Quando sono in piedi loro sono sdraiati, quando sono sdraiato loro sono in piedi. Chi sono? ", /* lista risposte*/{
+	/*8Â° domanda*/{"Quando sono in piedi loro sono sdraiati, quando sono sdraiato loro sono in piedi. Chi sono? ", /* lista risposte*/{
 						" a - I capelli",
 						" b - I vestiti",
 						" c - I gomiti",
-						" d - I piedi"	}, /* fine lista risposte 2° domanda*/ 
-		/*risposta esatta*/ 'd'},/*fine 8° domanda	
+						" d - I piedi"	}, /* fine lista risposte 2Â° domanda*/ 
+		/*risposta esatta*/ 'd'},/*fine 8Â° domanda	
 
-	/*9° domanda*/{"Lo puoi piantare, ma non crescerà; ha una testa ma mai ragionerà, chi è?", /* lista risposte*/{
+	/*9Â° domanda*/{"Lo puoi piantare, ma non crescerÃ ; ha una testa ma mai ragionerÃ , chi Ã¨?", /* lista risposte*/{
 						" a - Il moscerino",
 						" b - Il chiodo",
 						" c - Il seme",
-						" d - L'asso"	}, /* fine lista risposte 2° domanda*/ 
-		/*risposta esatta*/ 'b'},/*fine 9° domanda	
+						" d - L'asso"	}, /* fine lista risposte 2Â° domanda*/ 
+		/*risposta esatta*/ 'b'},/*fine 9Â° domanda	
 						
-	/*ULTIMA domanda*/{"Esisto fino a quando hai vita Ma se mi perdi non c'è più vita! ", /* lista risposte*/{
+	/*ULTIMA domanda*/{"Esisto fino a quando hai vita Ma se mi perdi non c'Ã¨ piÃ¹ vita! ", /* lista risposte*/{
 						" a - La speranza ",
 						" b - La gioia ",
 						" c - La tristezza",
@@ -82,15 +83,16 @@ using namespace std;
 	1) sceglie una domanda A CASO  tra le NUM_DOM
 	2) viualizza il testo della domanda e delle possibili risposte 
 	3) gestisce la risposta :  legge la risposta
-			controlla se è esatta 
-			ritorna TRUE se è OK FALSE se è errata
+			controlla se Ã¨ esatta 
+			ritorna TRUE se Ã¨ OK FALSE se Ã¨ errata
                 NOTA :    (SI POTREBBE PENSARE DI FARE GESTIRE LE AZIONI CORRISPONDENTI: 
 		 N PASSI AVANTI   o N PASSI INDIETRO  direttamente QUI)
 	*/
 
 bool gestione_domanda(){
-    int num_dom;   //indicherà il numero della domanda da fare  sarà  0<  num_dom  <NUM_TOT_DOM
+    int num_dom;   //indicherÃ  il numero della domanda da fare  sarÃ   0<  num_dom  <NUM_TOT_DOM
     char risp=' ';
+    effetti e;
 	
  	num_dom = (rand() % NUM_TOT_DOM) ;
 	cout<<" domanda num:  "   << num_dom<<endl;
@@ -117,12 +119,14 @@ if(risp==vect_dom[num_dom].risp_esatta){
 	cout<<" Risposta esatta !!! "<<endl<<endl;
 	cin.ignore();  // NON SO BENE SE SERVE???
 	// viene eventualmente invocata la funzione che gestisce la risposta esatta  oppure return true
+	e.vai_avanti(int pos);
 	return true;
 }else{
 // RISPOSTA ERRATA
-	cout<<" Risposta è errata  !!! "<<endl<<endl;
+	cout<<" Risposta Ã¨ errata  !!! "<<endl<<endl;
 	cin.ignore();  // NON SO BENE SE SERVE???
 	// viene eventualmente invocata la funzione che gestisce la risposta esatta  oppure return true
+	e.vai_indietro(int pos);
 	return false;
 }
 
