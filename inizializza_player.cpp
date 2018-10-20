0527515fcc716aefb8cecf31b0f06390aa974875
Martina <matry98@live.it>
2018-10-20 19:23:34 +0200

    Update inizializza_player.cpp

--- inizializza_player.cpp
+++ inizializza_player.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int inizializza_giocatori::decidi_numero_giocatori(){
 
-	cout << "decidi il numero dei giocatori: ";
+    //cout<<"State per addentrarvi nei mea";
+	cout << "Per iniziare decidi il numero dei giocatori: ";
             cin >> num_giocatori;                                           //numero giocatori
             if (num_giocatori < 2){
                 cout << "i giocatori devono essere almeno 2." << endl;
@@ -20,12 +21,25 @@ int inizializza_giocatori::return_num_giocatori(){
 }
 
 void inizializza_giocatori::inizializza_player(){
+	cout<<"\nSabah al khayr, buongiorno, io sono Anubi, dio dell'oltretomba e sorvegliero' il vostro viaggio nella buona e nella cattiva sorte"<<endl
+	    
+	    <<"Voi siete?"<<endl<<endl;
 	for(int i = 0; i < num_giocatori; i++){
-            cout << "inserisci nome giocatore " << i+1 << ": ";
+            cout << "(inserire nome giocatore) " << i+1 << ": ";
             cin>>N;
             posizione = 0;
             gioc[i] = new player(posizione, N);
         }
+        cout<<endl;
+    stampa_giocatori();
+    cout<<"\nBenvenuti nella Valle dei Re!"<<endl
+        <<"molti hanno cercato di arrivare al tesoro di Tutankamon, "
+		<<"ma nessuno fin ora e' riuscito nell'impresa, "
+		<<"chissa' che uno di voi non riesca a raggiungerlo. "
+		<<"Non indugiamo, prego..cominciamo "<<endl<<endl
+		<<"ma ricordate: "<<endl<<endl
+		<<"'La tempesta si alzera'. Il cielo si aprira'. Il potere si scatenera'' contro chi un passo falso commetera'"<<"\n"<<"\n";
+	cout<<"Haz saeed, buona fortuna!"<<"\n"<<"\n";
 }
 
 void inizializza_giocatori::stampa_giocatori(){
@@ -50,16 +64,17 @@ int inizializza_giocatori::return_turn(){
 void inizializza_giocatori::posizione_corrente(int num){
 	dado d;
 	    turno = 0;
-
+	 	termina = false;
+	 	char stampa;
         int t = set_turno();
         int j = 0;
         int fw_or_bw[num];
         //int pos_corr[1000]; 
         int i = 0;
-        
+        //CREO IL MAZZO
+        m.crea_mazzo();
         int num_caselle = return_caselle();
-        cout<<"numero caselle "<<num_caselle<<endl;
-	    
+        //cout<<"numero caselle "<<num_caselle<<endl;
 	   // tab.stampa_tabellone();
         while(gioc[i]->pos != num_caselle && termina == false){
 		 	genera_tabellone(num_caselle);
@@ -73,7 +88,7 @@ void inizializza_giocatori::posizione_corrente(int num){
 			        gioc[i]->pos = gioc[i]->vai_avanti_n_caselle(num_estratto);
 			    	
 			    	//cout<<"pos_corr[j] giocatore "<<i+1<<": ";
-			    	//CONDIZIONE CHE PERMETTE DI STABILIRE CHE UN GIOCATORE � ARRIVATO AL TRAGUARDO.
+			    	//CONDIZIONE CHE PERMETTE DI STABILIRE CHE UN GIOCATORE è ARRIVATO AL TRAGUARDO.
 		    		if(gioc[i]->pos == num_caselle){
 		    			cout<<"posizione giocatore "<<i+1<<"("<<gioc[i]->name<<"): "<<gioc[i]->pos<<endl;
 				    	cout<<"giocatore "<<i+1<<" SEI ARRIVATO! "<<endl;
@@ -91,7 +106,7 @@ void inizializza_giocatori::posizione_corrente(int num){
 						cout<<"partenza"<<endl;
 			    	
 			    	//CONDIZIONE VAI ALLA CASELLA N
-			    	else if (gioc[i]->pos%9==0){                 
+			    	else if (gioc[i]->pos%10==0){                 
 			    		gioc[i]->pos = e.vai_avanti(gioc[i]->pos);
 			    		cout<<"posizione giocatore "<<i+1<<"("<<gioc[i]->name<<"): "<<gioc[i]->pos<<endl;
 			    		cout<<"\n";
@@ -104,15 +119,16 @@ void inizializza_giocatori::posizione_corrente(int num){
 						cout<<"\n";
 					}
 					
-			    	else if(gioc[i]->pos%10==0){      //condizione casella pesca carta
-						e.pesca_carta();
-						gioc[i]->pos = gioc[i]->pos + gioc[i]->pos;
+					//CONDIZIONE PESCA CARTA
+			    	else if(gioc[i]->pos%4==0){ 
+						
+						gioc[i]->pos = effetti_carta(i);
 						cout<<"posizione giocatore: "<<gioc[i]->pos<<endl;
 					}
 			
 					
 					//CONDIZIONE CHE PERMETTE DI ANDARE AVANTI/INDIETRO RANDOM
-					else if(gioc[i]->pos%4==0 && t>1){
+					else if(gioc[i]->pos%9==0 && t>1){
 				
 						fw_or_bw[i] = rand()%2;
 						int num = e.fortuna(num_giocatori);
@@ -127,13 +143,38 @@ void inizializza_giocatori::posizione_corrente(int num){
 			            }
 						cout<<"NUOVA POSIZIONE "<<i+1<<": "<<gioc[i]->pos<<endl;
 				    } 
-					
+				    
+				        //CONDIZIONE SCAMBIA GIOCATORI
+				    /*	else if(gioc[i]->pos==13 || gioc[i]->pos == 23){
+							int pl = rand()%num_giocatori+1;
+							int temp;
+							if(pl!= i){
+								temp = gioc[i]->pos;
+								gioc[i]->pos = gioc[pl]->pos;
+								gioc[pl]->pos = temp;
+								cout<<"SCAMBIA CON "<<pl+1<<endl;
+								cout<<"posizione giocatore "<<i+1<<": "<<gioc[i]->pos<<endl;
+								cout<<"posizione giocatore "<<pl+1<<": "<<gioc[pl]->pos<<endl;
+						    }
+						    else {
+								cout<<"SEI STATO FORTUNATO, RIMANI AL TUO POSTO!! "<<endl;
+							    cout<<"posizione giocatore "<<i+1<<": "<<gioc[i]->pos<<endl;
+						    }  
+						}*/
+					else if(gioc[i]->pos==13 || gioc[i]->pos == 23){
+						cout<<"SEI IN PRIGIONE! STAI FERMO UN TURNO!"<<endl;
+						gioc[i]->pos = gioc[i]->pos - num_estratto;
+					}
 					//CASELLA VUOTA
 					else cout<<"posizione giocatore "<<i+1<<"("<<gioc[i]->name<<"): "<<gioc[i]->pos<<endl;        
 				
 					cout<<"\n";
 					} 
-		    stampa_tabellone();
+			cout<<"premi T per stampare tabellone.."<<endl;
+			cin>>stampa;
+			if(stampa=='t')
+		    	stampa_tabellone();
+		    
 		}  
 
 	}
@@ -152,22 +193,7 @@ void inizializza_giocatori::posizione_corrente(int num){
 			
 		}*/
 		//CONDIZIONE SCAMBIA POSTO GIOCATORI
-	/*	else if(gioc[i]->pos==13 || gioc[i]->pos == 23){
-			int pl = rand()%num_giocatori+1;
-			int temp;
-			if(pl!= i){
-			temp = gioc[i]->pos;
-			gioc[i]->pos = p[pl]->pos;
-			p[pl]->pos = temp;
-			cout<<"SCAMBIA CON "<<pl+1<<endl;
-			cout<<"posizione giocatore "<<i+1<<": "<<gioc[i]->pos<<endl;
-			cout<<"posizione giocatore "<<pl+1<<": "<<p[pl]->pos<<endl;
-		    }
-		    else {
-			cout<<"SEI STATO FORTUNATO, RIMANI AL TUO POSTO!! "<<endl;
-		    cout<<"posizione giocatore "<<i+1<<": "<<gioc[i]->pos<<endl;
-		    }  
-		}*/
+
 		
 		
 
@@ -197,14 +223,20 @@ void inizializza_giocatori::genera_tabellone(int num_cas){
 
 bool inizializza_giocatori::stampa_tabellone(){
 	int j;
+	termina = false;
 	j = return_num_giocatori();
 	int accapo = 0;
     while(head->next != NULL){
-            
+              for(int i = 0; i < j; i++){
+            if(gioc[i]->pos == head->val){
+                gioc[i]->presenta_giocatore();
+                cout<<"->";
+            }
+        }
             //stampa numeri
         if((head->next->next != NULL) && (head->val != 1)){
-            if(head->val < 10) cout << "|" << head->val << "|" << '\t' << '\t' << '\t' << '\t';
-            else cout << "|" << head->val << "|" << '\t' << '\t' << '\t';    //dispari
+            if(head->val < 10) cout << "|" << head->val << "|" << '\t' << '\t' << '\t';
+            else cout<<"|" << head->val << "|" << '\t' << '\t' << '\t';    //dispari
         }
             
             //stampa partenza e arrivo
@@ -214,16 +246,12 @@ bool inizializza_giocatori::stampa_tabellone(){
         head = head->next;
         accapo += 1;
             
-       for(int i = 0; i < j; i++){
-            if(gioc[i]->pos == head->val){
-                gioc[i]->presenta_giocatore();
-                cout<<"->";
-            }
-        }
+     
         
             //spaziatura a capo
-        if (accapo == 10){
-            cout << endl << endl << endl;
+        if (accapo == 5){
+            cout << endl <<endl<<endl;
+            cout<<"\t";
             accapo = 0;
             }
         }
@@ -244,3 +272,58 @@ int inizializza_giocatori::return_caselle(){
         return i;
 }
 
+
+int inizializza_giocatori::effetti_carta(int i){
+	//m.stampa_mazzo();
+	//cout<<"\n"<<"\n";
+	carta card;
+	
+	cout<<"CARTA ESTRATTA:"<<endl;
+	card = m.estrai_in_cima();
+	card.stampa_carta();
+	cout<<"\n";
+	int posiz; 
+	bool result;
+	bool fermo=false;
+	string msg = card.getmsg();
+
+	if(msg == "ritira il dado")
+		posiz = e.tira_di_nuovo() + gioc[i]->pos; 
+				
+	else if(msg == "rispondi alla domanda"){
+		cout<<"La sfinge si è svegliata, rispondi a sta cagataaaaaa yeee!!!!  "<<endl;
+		result = m.gestione_domanda();
+		if(result == true)
+			posiz = gioc[i]->avanza_uno();
+		else
+			posiz = gioc[i]->indietro_uno();
+	}	
+	else if(msg == "ritorna alla casella 0")
+		posiz = gioc[i]->ritorna_pos()-gioc[i]->ritorna_pos();	
+		  	    
+	else if(msg == "fai 3 passi in avanti")
+		posiz = gioc[i]->vai_avanti_n_caselle(3);  
+		  	    
+	else if(msg == "fai 3 passi indietro") 
+		posiz = gioc[i]->indietro_n_caselle(3);	
+		  	
+   else if (msg == "scambia posizione con un altro giocatore"){
+   		int pl = rand()%num_giocatori+1;
+		int temp;
+		if(pl!= i){
+			temp = gioc[i]->pos;
+			posiz = gioc[pl]->pos;
+			gioc[pl]->pos = temp;
+			cout<<"SCAMBIA CON "<<pl+1<<endl;
+			cout<<"posizione giocatore "<<i+1<<": "<<posiz<<endl;
+			cout<<"posizione giocatore "<<pl+1<<": "<<gioc[pl]->pos<<endl;
+	    }
+	    else {
+			cout<<"SEI STATO FORTUNATO, RIMANI AL TUO POSTO!! "<<endl;
+		    cout<<"posizione giocatore "<<i+1<<": "<<gioc[i]->pos<<endl;
+						    }  
+						}
+		
+	return (posiz);
+	
+}
