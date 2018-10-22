a6145ba89361280efc7632ba492245545085d86e
Martina <matry98@live.it>
2018-10-22 23:23:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include "inizializza_player.h"
+#include "inizializza_gioco.h"
 
 
 
@@ -9,65 +9,23 @@ using namespace std;
 
 int main(){
 	char start;
-    inizializza_giocatori init;
+    inizializza_gioco init;
     init.decidi_numero_giocatori();
     init.inizializza_player();
-   // init.stampa_giocatori();
     int num_player = init.return_num_giocatori();
-    // int num_turno = 0;
-    // domanda d;
-     //d.gestione_domanda();
    
-
-    
-/*   tabellone t;
-   cout<<t.return_caselle();
-//    t.genera_tabellone(40);
-  //  t.stampa_tabellone();
-    cout << endl;
-
- */ 
-    srand(time(0));       //permette di generare vari numeri, in un loop non funziona.
-    
-    
-/*    mazzo m;
-    m.crea_mazzo();
-    m.stampa_mazzo();
-   // m.gestione_domanda();
-   // m.pesca_carta();
-    m.mischia_mazzo();
-    cout<<"MISCHIO MAZZO "<<"\n";
-    m.stampa_mazzo();
-    m.mischia_mazzo();
-    cout<<"MISCHIO MAZZO "<<"\n";
-    m.stampa_mazzo();
-    m.mischia_mazzo();
-    cout<<"MISCHIO MAZZO "<<"\n";
-    m.stampa_mazzo();
-  /*  cout<<"SCAMBIO CARTEEEE"<<"\n"<<"\n";
-    m.scambia_carta(2, 4);
-    m.stampa_mazzo();
-    cout<<"\n"<<"\n";
-    cout<<"estraggo carta 2: ";
-    m.estrai_carta(2);
-*/	
    
+    srand(time(0));       //permette di generare numeri random
     
-    //turno turn;
-    //turn.inizio_turno(num_player);
-    /*num_turno = init.return_turn();
-    while(num_turno<8){
-    init.posizione_corrente(num_player);
-    num_turno ++;
-}*/
-	cout<<"premi a per iniziare "<<endl;
-	cin>>start;
-	if(start=='a')
-    	init.posizione_corrente(num_player);
-   
+    
+    while(start != 'a'){
+		cout<<"premi a per iniziare "<<endl;
+		cin>>start;
+		if(start=='a')
+			init.fai_turno(num_player);
+		else 
+			cout<<"INPUT NON VALIDO"<<"\n";
+   	}
      
-   /* domanda d;
-    gestione_domanda(d);
-  */  
     return 0;
 }
