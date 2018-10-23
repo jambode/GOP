c243868ab01ee8a38f5aa552f1c9d6b436f299f8
Martina <matry98@live.it>
2018-10-23 10:34:23 +0200

    Update player.h

--- player.h
+++ player.h
@@ -2,20 +2,18 @@
 #define _PLAYER_H_
 #include <cstring>
 #include <iostream>
-#include "dado.h"
+
 
 using namespace std;
 
 class player{
 protected:
    
+    
     int turno ;
 public: 
     int pos;
-     char name[50];
-    //dado d;
-    //player();
-
+    char name[50];
     player(int p, char n[]);
     void presenta_giocatore();
     
@@ -28,7 +26,7 @@ public:
     int vuota();
     int somma_pos(int n);
     int indietro_n_caselle(int i);
-    //fermo, scambia pos.
+   
 };
 #include "player.cpp"
 #endif /* player_h */
