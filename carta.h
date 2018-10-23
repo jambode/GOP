ab6c01b636dca520baaccdc4fda2aec4fe87e96d
Martina <matry98@live.it>
2018-10-23 10:26:17 +0200

    Update carta.h

--- carta.h
+++ carta.h
@@ -12,7 +12,7 @@ using namespace std;
 class carta{
    protected:
 		int valore;   // i valori  sono cmpresi tra 0 e 5  .....i valori possibili sono 0,1,2,3,4,5
-		string msg; // frase che indica l'azione da fare  DA VEDERE SE SERVE
+		string msg; // messaggio che indica l'azione da fare  
 	public:
 		carta(){};	
 		// SET
@@ -20,7 +20,7 @@ class carta{
 		void set_message(string m);
 		//GET
 		int getvalore();	// restituisce il valore della carta
-		string getmsg();  // restituisce il msg associato alla carta
+		string getmsg();  // restituisce il messaggio associato alla carta
          //TEST
 		void stampa_carta(); //stampa il messaggio della carta 
 
