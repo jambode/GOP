b278d848e7508d155a033b1fca48261b2cf03b21
Martina <matry98@live.it>
2018-10-23 10:27:25 +0200

    Update mazzo_carte.h

--- mazzo_carte.h
+++ mazzo_carte.h
@@ -2,16 +2,18 @@
 #define MAZZO_CARTE_H
 #include <cstring>
 #include <iostream>
-#include "carta.h"
+#include<ctime> // per time()
+#include<cstdlib>
 #define NUM_RISP 4	// numero possibili risposte per ogni domanda
 #define NUM_TOT_DOM 10	// numero totale domande  20  per test  3
+#include "carta.h"
 #include "effetti.h"
 #include "player.h"
 using namespace std;
 
 
 
-// il mazzo viene implementato come una classe :un array di cartE E I vari metodi
+// il mazzo viene implementato come una classe :un array di carte e i vari metodi
 
 
 struct domanda{
@@ -26,28 +28,27 @@ struct domanda{
 class mazzo{
     protected: 
         carta carte[NUMCARTE];
-	    int  num_cartacorrente;
 	   
 
-public:
-	carta temp;
-	player *p;
-	effetti e;
-	//domanda d;
-	mazzo();  // COSTRUTTORE
-	void crea_mazzo();
-	void mischia_mazzo();
-	void scambia_carta(int i, int J);
-	carta estrai_in_cima(); // estrae la carta dalla prima posizione poi la mette all'ultimo
-							//posto facendo slittare tutti i valori di un posto in avanti
-	void inserisci_in_fondo(); // NON IMPLEMENTATA DA TOGLIERE
-	string msg_carte(int num);
-// servono  per i test sulla classe mazzo	
-	void stampa_mazzo();   
-	carta estrai_carta(int i);
-	bool gestione_domanda();
-	int pesca_carta();
-	};
+	public:
+		carta temp;
+		player *p;
+		effetti e;
+		
+		mazzo();  // COSTRUTTORE
+		void crea_mazzo();
+		void mischia_mazzo();
+		void scambia_carta(int i, int J);
+		carta estrai_in_cima(); // estrae la carta dalla prima posizione poi la mette all'ultimo
+				        //posto facendo slittare tutti i valori di un posto in avanti
+								
+		
+		string msg_carte(int num); 	
+		void stampa_mazzo();   
+		carta estrai_carta(int i);
+		bool gestione_domanda();
+		int pesca_carta();
+		};
 
 #include "mazzo_carte.cpp"
 #endif /* mazzo_carte_h */
