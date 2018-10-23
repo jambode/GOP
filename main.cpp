d4ff82dd0c98aaec1e14a5c47ea7782658e2ba50
Martina <matry98@live.it>
2018-10-23 14:54:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,12 @@
 #include <ctime>
 #include <cstdlib>
 #include "inizializza_gioco.h"
-
-
-
 using namespace std;
 
 int main(){
-	char start;
+	string start;
     inizializza_gioco init;
+    init.regole();
     init.decidi_numero_giocatori();
     init.inizializza_player();
     int num_player = init.return_num_giocatori();
@@ -18,10 +16,10 @@ int main(){
     srand(time(0));       //permette di generare numeri random
     
     
-    while(start != 'a'){
+    while(start != "a"){
 		cout<<"premi a per iniziare "<<endl;
 		cin>>start;
-		if(start=='a')
+		if(start=="a")
 			init.fai_turno(num_player);
 		else 
 			cout<<"INPUT NON VALIDO"<<"\n";
