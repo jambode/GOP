548f247d798197a320f86b3641e81046e1a9d593
Martina <matry98@live.it>
2018-10-23 14:52:22 +0200

    Update effetti.cpp

--- effetti.cpp
+++ effetti.cpp
@@ -3,27 +3,24 @@
 using namespace std;
  
  
- 
+ //lancia il dado una seconda volta
  int effetti::tira_di_nuovo(){
  		int dice = d.lancia_dado();
  		cout<<"lancio dado seconda volta.."<<dice<<endl;
  		return dice;
  }
  
- int effetti::vai_avanti(int pos){
- 	int dice = d.lancia_dado();
- 	cout<<"CASELLA FORTUNATA!! "<<"avanza di "<<dice<<endl;
- 	pos =	pos+dice;
- 	return pos;
+ //il giocatore viene spostato in una casella in modo random
+ int effetti::vai_avanti(int num){
+ 	int num_cas = rand()%num+1;
+ 	cout<<"SABBIE MOBILI, ATTENZIONE! Vai alla casella "<<num_cas<<endl;
+ 	return num_cas;
  }
 
 
  //RITORNA UN NUMERO E IL GIOCATORE VA AVANTI/INDIETRO IN BASE A QUEL NUMERO
 int effetti::fortuna(int num){
 	cout<<"FORTUNA O SFORTUNA? ";
-	//cout<<"FORTUNA O SFORTUNA? ";   messo nella funzione a parte
- 	
 	int player_pos = rand()%num+1;
- 	
  	return player_pos;
  }
